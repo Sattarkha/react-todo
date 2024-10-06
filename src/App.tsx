dc37d89770d73fbdbd01426b98e8e297fc81144d
abdul sattar <156591821+Sattarkha@users.noreply.github.com>
2024-10-06 12:37:34 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+const TodoList =() => {
+  return(
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <form>
+        <input type="text" placeholder="Add new todo" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        <li>
+          <span>Todo1</span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+        <li> 
+          <span>Todo2</span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+        <li>  
+          <span>Todo3</span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default TodoList;
